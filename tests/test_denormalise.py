fa29285eb28d70d27f642d1a1becbc16f0b8dd64
Owen Campbell <owen.campbell@uk.python.org>
2017-08-14 11:01:21 +0100

    Update tests for yaml layout with venue availability

--- tests/test_denormalise.py
+++ tests/test_denormalise.py
@@ -14,22 +14,37 @@ def event_types():
 
 @pytest.fixture
 def days(event_types):
-    return {
-        datetime(2017, 10, 26, 0, 0): {'event_types': event_types},
-        datetime(2017, 10, 27, 0, 0): {'event_types': event_types},
-        datetime(2017, 10, 28, 0, 0): {'event_types': event_types},
-        datetime(2017, 10, 29, 0, 0): {'event_types': event_types},
-    }
+    return [
+        datetime(2017, 10, 26, 0, 0),
+        datetime(2017, 10, 27, 0, 0),
+        datetime(2017, 10, 28, 0, 0),
+        datetime(2017, 10, 29, 0, 0),
+    ]
 
 
 @pytest.fixture
-def venues():
+def venues(days):
     return {
-        'Assembly Room': {'capacity': 500, 'suitable_for': ['talk', 'plenary']},
-        'Room A': {'capacity': 80, 'suitable_for': ['workshop']},
-        'Ferrier Hall': {'capacity': 200, 'suitable_for': ['talk']},
-        'Room C': {'capacity': 80, 'suitable_for': ['talk', 'workshop']},
-        'Room D': {'capacity': 100, 'suitable_for': ['talk']}
+        'Assembly Room': {
+            'capacity': 500,
+            'suitable_for': ['talk', 'plenary'],
+            'available': days},
+        'Room A': {
+            'capacity': 80,
+            'suitable_for': ['workshop'],
+            'available': days},
+        'Ferrier Hall': {
+            'capacity': 200,
+            'suitable_for': ['talk'],
+            'available': days},
+        'Room C': {
+            'capacity': 80,
+            'suitable_for': ['talk', 'workshop'],
+            'available': days},
+        'Room D': {
+            'capacity': 100,
+            'suitable_for': ['talk'],
+            'available': [datetime(2017, 10, 26, 0, 0)]}
     }
 
 
@@ -147,7 +162,7 @@ def test_slot_times(event_types, session_times, slot_times):
 def test_slots(event_types, venues, days, slot_times):
     slots = dn.slots(event_types, venues, days, slot_times)
     assert all(isinstance(slot, Slot) for slot in slots)
-    assert len(slots) == 84
+    assert len(slots) == 72
 
 
 def test_events(event_types, events_definition):
