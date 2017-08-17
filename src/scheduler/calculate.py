874f2a7b44a9815b3c38d3d93498ce79b08e24f7
Owen Campbell <owen.campbell@uk.python.org>
2017-08-17 08:16:05 +0100

    Refactor solvers and kwargs

--- src/scheduler/calculate.py
+++ src/scheduler/calculate.py
@@ -7,49 +7,40 @@ from pulp import PULP_CBC_CMD
 
 logger = daiquiri.getLogger(__name__)
 
+solvers = {
+    'pulp_cbc_cmd': {
+        'function': scheduler.solution,
+        'kwargs': {
+            'objective_function': None,
+            'solver': PULP_CBC_CMD(msg=False)}},
+    'glpk': {
+        'function': scheduler.solution,
+        'kwargs': {
+            'solver': GLPK(msg=False)}},
+    'hill_climber': {
+        'function': scheduler.heuristic,
+        'kwargs': {
+            'objective_function': None,
+            'algorithm': hill_climber}},
+    'simulated_annealing': {
+        'function': scheduler.heuristic,
+        'kwargs': {
+            'objective_function': None,
+            'algorithm': simulated_annealing}}}
+
 
 def solution(events, slots, solver):
     logger.info(f'Scheduling conference using {solver} solver....')
-    solvers = {
-        'pulp_cbc_cmd': {
-            'function': scheduler.solution,
-            'kwargs': {
-                'events': events,
-                'slots': slots,
-                'objective_function': None,
-                'solver': PULP_CBC_CMD(msg=False)
-            }
-        },
-        'glpk': {
-            'function': scheduler.solution,
-            'kwargs': {
-                'events': events,
-                'slots': slots,
-                'objective_function': None,
-                'solver': GLPK(msg=False)
-            }
-        },
-        'hill_climber': {
-            'function': scheduler.heuristic,
-            'kwargs': {
-                'events': events,
-                'slots': slots,
-                'objective_function': None,
-                'algorithm': hill_climber
-            }
-        },
-        'simulated_annealing': {
-            'function': scheduler.heuristic,
-            'kwargs': {
-                'events': events,
-                'slots': slots,
-                'objective_function': None,
-                'algorithm': simulated_annealing
-            }
-        },
+
+    common_kwargs = {
+        'events': events,
+        'slots': slots,
     }
+
+    kwargs = {**common_kwargs, **solvers[solver]['kwargs']}
+
     try:
-        solution = solvers[solver]['function'](**solvers[solver]['kwargs'])
+        solution = solvers[solver]['function'](**kwargs)
     except ValueError:
         logger.error('No valid solution found')
         solution = None
