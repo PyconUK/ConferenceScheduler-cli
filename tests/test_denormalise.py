3acf6c2d36dbe61603b621358931918e42ab63b5
Owen Campbell <owen.campbell@uk.python.org>
2017-08-17 11:54:49 +0100

    Fix tests for new input format

--- tests/test_denormalise.py
+++ tests/test_denormalise.py
@@ -13,80 +13,40 @@ def event_types():
 
 
 @pytest.fixture
-def days(event_types):
-    return [
-        datetime(2017, 10, 26, 0, 0),
-        datetime(2017, 10, 27, 0, 0),
-        datetime(2017, 10, 28, 0, 0),
-        datetime(2017, 10, 29, 0, 0),
-    ]
-
-
-@pytest.fixture
-def venues(days):
+def venues():
     return {
         'Assembly Room': {
-            'capacity': 500,
-            'suitable_for': ['talk', 'plenary'],
-            'available': days},
-        'Room A': {
-            'capacity': 80,
-            'suitable_for': ['workshop'],
-            'available': days},
-        'Ferrier Hall': {
-            'capacity': 200,
-            'suitable_for': ['talk'],
-            'available': days},
-        'Room C': {
-            'capacity': 80,
-            'suitable_for': ['talk', 'workshop'],
-            'available': days},
-        'Room D': {
-            'capacity': 100,
-            'suitable_for': ['talk'],
-            'available': [datetime(2017, 10, 26, 0, 0)]}
-    }
-
-
-@pytest.fixture
-def session_times():
-    return {
-        'plenary': {'None': [{'starts_at': 34200, 'duration': 45}]},
-        'talk': {
-            'morning': [
-                {'starts_at': 36900, 'duration': 30},
-                {'starts_at': 39600, 'duration': 30}
-            ],
-            'afternoon': [
-                {'starts_at': 52200, 'duration': 30},
-                {'starts_at': 54000, 'duration': 30}
-            ]
-        },
-        'workshop': {
-            'None': [
-                {'starts_at': 36900, 'duration': 120},
-                {'starts_at': 52200, 'duration': 120}
-            ],
+            datetime(2017, 10, 26, 0, 0): {
+                'morning': [
+                    {
+                        'starts_at': 36900,
+                        'duration': 30,
+                        'event_type': 'plenary',
+                        'capacity': 600
+                    },
+                    {
+                        'starts_at': 39600,
+                        'duration': 30,
+                        'event_type': 'plenary',
+                        'capacity': 600
+                    },
+                ],
+                'afternoon': [
+                    {
+                        'starts_at': 52200,
+                        'duration': 30,
+                        'event_type': 'talk',
+                        'capacity': 600
+                    },
+                    {
+                        'starts_at': 54000,
+                        'duration': 30,
+                        'event_type': 'talk',
+                        'capacity': 600
+                    }
+                ]
+            }
         }
-    }
-
-
-@pytest.fixture
-def slot_times(session_times):
-    return {
-        'plenary': [
-            {'duration': 45, 'session_name': 'None', 'starts_at': 34200}
-        ],
-        'talk': [
-            {'duration': 30, 'session_name': 'morning', 'starts_at': 36900},
-            {'duration': 30, 'session_name': 'morning', 'starts_at': 39600},
-            {'duration': 30, 'session_name': 'afternoon', 'starts_at': 52200},
-            {'duration': 30, 'session_name': 'afternoon', 'starts_at': 54000}
-        ],
-        'workshop': [
-            {'duration': 120, 'session_name': 'None', 'starts_at': 36900},
-            {'duration': 120, 'session_name': 'None', 'starts_at': 52200}
-        ]
     }
 
 
@@ -155,28 +115,23 @@ def clashes_definition():
     }
 
 
-def test_slot_times(event_types, session_times, slot_times):
-    assert dn.slot_times(event_types, session_times) == slot_times
+def test_types_and_slots(event_types, venues):
+    types_and_slots = dn.types_and_slots(venues)
+    assert all(isinstance(item['slot'], Slot) for item in types_and_slots)
+    assert len(types_and_slots) == 4
 
 
-def test_slots(event_types, venues, days, slot_times):
-    slots = dn.slots(event_types, venues, days, slot_times)
-    assert all(isinstance(item['slot'], Slot) for item in slots)
-    assert len(slots) == 72
-
-
-def test_events(event_types, events_definition):
-    events = dn.events(events_definition)
-    assert all(isinstance(item['event'], Event)for item in events)
-    assert len(events) == 6
+def test_types_and_events(events_definition):
+    types_and_events = dn.types_and_events(events_definition)
+    assert all(isinstance(item['event'], Event)for item in types_and_events)
+    assert len(types_and_events) == 6
 
 
 def test_unavailability(
-    event_types, events_definition, venues, days, slot_times,
-    unavailability_defintion
+    event_types, events_definition, venues, unavailability_defintion
 ):
-    slots = [item['slot']
-             for item in dn.slots(event_types, venues, days, slot_times)]
+    types_and_slots = dn.types_and_slots(venues)
+    slots = [item['slot'] for item in types_and_slots]
     unavailability = dn.unavailability(
         events_definition, slots, unavailability_defintion)
     print(unavailability)
@@ -185,13 +140,10 @@ def test_unavailability(
 
 def test_clashes(events_definition, clashes_definition):
     clashes = dn.clashes(events_definition, clashes_definition)
-    assert clashes == {0: [2, 3, 1], 1: [2, 3, 0], 2: [0, 1], 3: [], 4: [], 5: []}
+    assert clashes == ({0: [2, 3, 1], 1: [2, 3, 0], 2: [0, 1], 3: [], 4: [], 5: []}, 2)
 
 
-def test_unsuitability(
-    venues, event_types, days, slot_times, events_definition
-):
-    slots = [item['slot']
-             for item in dn.slots(event_types, venues, days, slot_times)]
-    unsuitability = dn.unsuitability(venues, slots, events_definition)
+def test_unsuitability(venues, events_definition):
+    types_and_slots = dn.types_and_slots(venues)
+    unsuitability = dn.unsuitability(types_and_slots, events_definition)
     assert list(unsuitability.keys()) == list(range(6))
